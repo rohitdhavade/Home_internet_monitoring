32b2cfd523b489a9da6e9685fbe071710c97639a
rohitdhavade <rohitdhavade@gmail.com>
2020-10-04 20:20:13 +0530

    edited Subscriber_Excel.py to add more graphs

--- Subscriber_Excel.py
+++ Subscriber_Excel.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from openpyxl import Workbook
 from openpyxl.chart import LineChart, BarChart, Reference, Series
-from datetime import date, timedelta  
+from datetime import date, timedelta, datetime
 from openpyxl.chart.axis import DateAxis
 from openpyxl.chart.axis import NumericAxis, TextAxis, SeriesAxis, DateAxis, ChartLines
 
 wb = Workbook()
 ws = wb.create_sheet(title="Report")
-dest_filename = 'Report.xlsx'
+dateandtime = datetime.now().strftime("%d.%m.%Y-%H.%M.%S")
+dest_filename = f'Report-{dateandtime}.xlsx'
 ws.append(['Date Time','Reachability Percent for selected interval'])
 x_ax_date = "%d/%m-%H:%M" #specifies format for date in graph
 
@@ -17,6 +18,11 @@ class Subscriber_Excel(SubscriberInterface):
     
     def __init__(self, uptime_dict:dict):
         self.uptime_dict = uptime_dict
+        self.points_added:int = 0
+        self.minrow:int = 1
+        self.anchor_point:int = 1
+        self.chart_height:int = 8
+        self.chart_width:int = 25
 
     def update(self, publisher):
         self.uptime_dict = publisher.get_state()
@@ -34,27 +40,36 @@ class Subscriber_Excel(SubscriberInterface):
             print('We didn\'t get permission to write to excel file, may it is open somewhere.')
     def update_excel_graph(self):
         self.update_excel()
-        values = Reference(ws, min_col=2, min_row=2, max_row=ws.max_row)
+        x_axis_max_scale = 70
+        if self.points_added == x_axis_max_scale:
+            self.minrow = self.minrow + self.points_added
+            self.points_added = 0
+            self.anchor_point = self.anchor_point + 2*self.chart_height + 4
+        values = Reference(ws, min_col=2, min_row="%s" %self.minrow, max_row=ws.max_row)
         chart = LineChart()
-        chart.height=8
-        chart.width=25
-        chart.style = 12
+        chart.height=self.chart_height
+        chart.width=self.chart_width
+        chart.style = 13
         chart.y_axis.crossAx = 500
         chart.y_axis.title = 'Avg Uptime'
         chart.x_axis = DateAxis(crossAx=100)
         chart.x_axis.title = 'Date'
-        chart.x_axis.scaling.max = 50
+        chart.x_axis.scaling.max = x_axis_max_scale
         chart.x_axis.scaling.min = 1
+        chart.x_axis.majorTickMark = 'cross' 
+        # as per our scaling.max value the tickers will be shown on x-axis
+        # if scaling.max = 70, then 70 tickers will be shown on x-axis.
 
-        chart.x_axis.minorGridlines = ChartLines()
-        chart.y_axis.minorGridlines = ChartLines()
+        chart.x_axis.majorGridlines = ChartLines()
+        chart.y_axis.majorGridlines = ChartLines()
 
         chart.add_data(values, titles_from_data=True)
-        dates = Reference(ws, min_col=1, min_row=2, max_row=ws.max_row)
+        dates = Reference(ws, min_col=1, min_row="%s" %self.minrow, max_row=ws.max_row)
         chart.set_categories(dates) # dates set a x-axis
 
         try:
-            ws.add_chart(chart, "E1") # 'E1' is anchor pint where graph starts in sheet
+            ws.add_chart(chart, "E%s" %self.anchor_point) # e.g.'E1' is anchor pint where graph starts in sheet
             wb.save(filename=dest_filename)
+            self.points_added += 1
         except PermissionError:
             print('We didn\'t get permission to write to excel file, may it is open somewhere.')
--- temp.py
+++ temp.py
@@ -1,61 +1,54 @@
 # # THIS FILE SERVES NO PURPOSE IN THE ACTUAL CODE, ONLY USED FOR TESTING.
 
-# from datetime import datetime, timedelta, date
-# from openpyxl import Workbook
-# import time
-# from openpyxl.chart import (
-#     LineChart,
-#     Reference,
-# )
-# from openpyxl.chart.axis import (NumericAxis, TextAxis, 
-# SeriesAxis, DateAxis, ChartLines, NestedInteger)
+from datetime import datetime, timedelta, date
+from openpyxl import Workbook
+import time
+from openpyxl.chart import (
+    LineChart,
+    Reference,
+)
+from openpyxl.chart.axis import (NumericAxis, TextAxis, 
+SeriesAxis, DateAxis, ChartLines, NestedInteger)
 
-# time.sleep(1)
-# wb = Workbook()
-# ws = wb.active
+time.sleep(1)
+wb = Workbook()
+ws = wb.active
 
-# rows = [
-#     ['Date', 'Batch 1', 'Batch 2', 'Batch 3'],
-#     [datetime.now().strftime("%m/%d/%Y-%H:%M:%S"), 40, 30, 25],
-#     [(datetime.now()+timedelta(seconds=1)).strftime("%m/%d/%Y-%H:%M:%S"), 40, 25, 30],
-#     [(datetime.now()+timedelta(seconds=2)).strftime("%m/%d/%Y-%H:%M:%S"), 50, 30, 45],
-#     [(datetime.now()+timedelta(seconds=3)).strftime("%m/%d/%Y-%H:%M:%S"), 30, 25, 40],
-#     [(datetime.now()+timedelta(seconds=4)).strftime("%m/%d/%Y-%H:%M:%S"), 25, 35, 30],
-#     [(datetime.now()+timedelta(seconds=5)).strftime("%m/%d/%Y-%H:%M:%S"), 20, 40, 35],
-# ]
-# for row in rows:
-#     ws.append(row)
+rows = [
+    ['Date', 'Batch 1', 'Batch 2', 'Batch 3'],
+    [datetime.now().strftime("%m/%d/%Y-%H:%M:%S"), 40, 30, 25],
+    [(datetime.now()+timedelta(seconds=1)).strftime("%m/%d/%Y-%H:%M:%S"), 40, 25, 30],
+    [(datetime.now()+timedelta(seconds=2)).strftime("%m/%d/%Y-%H:%M:%S"), 50, 30, 45],
+    [(datetime.now()+timedelta(seconds=3)).strftime("%m/%d/%Y-%H:%M:%S"), 30, 25, 40],
+    [(datetime.now()+timedelta(seconds=4)).strftime("%m/%d/%Y-%H:%M:%S"), 25, 35, 30],
+    [(datetime.now()+timedelta(seconds=5)).strftime("%m/%d/%Y-%H:%M:%S"), 20, 40, 35],
+]
+for row in rows:
+    ws.append(row)
 
-# data = Reference(ws, min_col=2, min_row=1, max_col=4, max_row=ws.max_row)
+data = Reference(ws, min_col=2, min_row=1, max_col=4, max_row=ws.max_row)
 
-# c2 = LineChart()
-# c2.title = "Date Axis"
-# c2.style = 13
-# c2.y_axis.title = "Size"
-# c2.y_axis.crossAx = 500 # this is mandatory
-# c2.x_axis = DateAxis(crossAx=100) # this ia mandatory
-# c2.x_axis.number_format = 'dd/mm/yy-HH:MM'
-# c2.x_axis.majorTimeUnit = "days"
-# c2.x_axis.title = "Date"
-# c2.x_axis.scaling.min = 2
-# c2.x_axis.scaling.max = 7
-# #c2.x_axis.minorGridlines = ChartLines()
-# c2.x_axis.delete = False
-# c2.y_axis.delete = False
+c2 = LineChart()
+c2.title = "Date Axis"
+c2.style = 13
+c2.y_axis.title = "Size"
+c2.y_axis.crossAx = 500 # this is mandatory
+c2.x_axis = DateAxis(crossAx=100) # this ia mandatory
+c2.x_axis.number_format = 'dd/mm/yy-HH:MM'
+c2.x_axis.majorTimeUnit = "days"
+c2.x_axis.title = "Date"
+c2.x_axis.scaling.min = 2
+c2.x_axis.scaling.max = 10
+c2.x_axis.minorGridlines = ChartLines()
+c2.x_axis.majorTickMark = 'cross'
 
-# c2.add_data(data, titles_from_data=True)
+c2.add_data(data, titles_from_data=True)
 
-# # in below we tell where the dates are, in our example dates are in 
-# # 1st column
-# dates = Reference(ws, min_col=1, min_row=2, max_row=ws.max_row)
-# c2.set_categories(dates)
+# in below we tell where the dates are, in our example dates are in 
+# 1st column
+dates = Reference(ws, min_col=1, min_row=2, max_row=ws.max_row)
+c2.set_categories(dates)
 
-# ws.add_chart(c2, "F1")
-# wb.save("line.xlsx")
-
-from pythonping import ping
-
-
-ping('127.0.0.1', verbose=True)
-
+ws.add_chart(c2, "F1")
+wb.save("line.xlsx")
 
